f1a91169f0894b32e02b8a10285c12b8e7e8b8c5
Roland <rolandosalajr@gmail.com>
2025-08-10 21:53:14 +0800

    initial deploy

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="dashboard">
+        <h1>Dashboard</h1>
+        <ul>
+            <li v-for="item in data" :key="item.id">{{ item.firstname }} {{ item.lastname }}</li>
+        </ul>
+        <button @click="fetchData">Refresh Data</button>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            data: []
+        }
+    },
+    onMounted() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://localhost:3000/fetchdata')
+                .then(response => {
+                    this.data = response.data;
+                    console.log(this.data)
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
+    }
+
+}
+</script>
